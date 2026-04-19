c235af51110ba4052caca72954605c75867e0470
ksjgusdh123 <122209567+ksjgusdh123@users.noreply.github.com>
2026-04-19 23:08:35 +0900

    잘못된 장착 시 흔들림 모션 적용

--- PocoPoachers/Assets/01. Scripts/UI/Inventory/DropSlotUI.cs
+++ PocoPoachers/Assets/01. Scripts/UI/Inventory/DropSlotUI.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -7,26 +8,40 @@ using UnityEngine.UI;
 // 장착 슬롯 공통 기반 클래스 - 상속받아 슬롯별 장착 로직 구현
 public class DropSlotUI : MonoBehaviour, IDropHandler
 {
+    [SerializeField] ItemType _itemType;
     [SerializeField] private Image _icon;
     [SerializeField] private TextMeshProUGUI _text;
 
     private ItemData _droppedItemData;
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
         if (DragHandler.SelectedItemSlot == null) return;
         ItemData prev = _droppedItemData;
-        OnItemDropped(DragHandler.SelectedItemSlot.SlotItemData);
+        if (!OnItemDropped(DragHandler.SelectedItemSlot.SlotItemData))
+        {
+            _rectTransform.DOKill();
+            _rectTransform.DOShakeAnchorPos(0.4f, strength: new Vector2(10f, 0f), vibrato: 20, randomness: 0);
+            return;
+        }
         DragHandler.SelectedItemSlot.EquipItem(prev);
     }
 
-    protected virtual void OnItemDropped(ItemData data)
+    protected virtual bool OnItemDropped(ItemData data)
     {
+        if (data.ItemType != _itemType) return false;
         _droppedItemData = data;
         _text.text = data.ItemName;
         _text.enabled = true;
         _icon.sprite = data.Icon;
         _icon.enabled = true;
+        return true;
     }
 
     // 장착 해제 시 호출
