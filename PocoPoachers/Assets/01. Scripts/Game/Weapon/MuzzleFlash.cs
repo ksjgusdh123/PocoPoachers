669477fb6a7adbc0d93573a640e18c405b9261b6
ljh010312 <leejunho32@naver.com>
2026-05-21 18:02:40 +0900

    MuzzleFlash Spot Light 추가

--- PocoPoachers/Assets/01. Scripts/Game/Weapon/MuzzleFlash.cs
+++ PocoPoachers/Assets/01. Scripts/Game/Weapon/MuzzleFlash.cs
@@ -1,14 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.VFX;
 
 [RequireComponent(typeof(VisualEffect))]
 public class MuzzleFlash : MonoBehaviour
 {
+    [SerializeField] private Light _flashLight;
+    [SerializeField] private float _flashDuration = 0.05f;
+    [SerializeField] private float _intensity = 5f;
+
     private VisualEffect _visualEffect;
+    private Coroutine _flashCoroutine;
 
     private void Awake()
     {
         _visualEffect = GetComponent<VisualEffect>();
+        if (_flashLight != null) _flashLight.enabled = false;
     }
 
     public void Play()
@@ -18,5 +25,18 @@ public class MuzzleFlash : MonoBehaviour
         _visualEffect.Stop();
         _visualEffect.Reinit();
         _visualEffect.SendEvent("OnPlay");
+
+        if (_flashLight == null) return;
+
+        if (_flashCoroutine != null) StopCoroutine(_flashCoroutine);
+        _flashCoroutine = StartCoroutine(FlashRoutine());
+    }
+
+    private IEnumerator FlashRoutine()
+    {
+        _flashLight.intensity = _intensity;
+        _flashLight.enabled = true;
+        yield return new WaitForSeconds(_flashDuration);
+        _flashLight.enabled = false;
     }
 }
