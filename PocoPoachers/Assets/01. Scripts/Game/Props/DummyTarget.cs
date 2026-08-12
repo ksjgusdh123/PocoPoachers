90164f8518eda60cd94c7885626bde954c0a8ec0
ljh010312 <leejunho32@naver.com>
2026-08-12 19:21:49 +0900

    더미 로직 수정

--- PocoPoachers/Assets/01. Scripts/Game/Props/DummyTarget.cs
+++ PocoPoachers/Assets/01. Scripts/Game/Props/DummyTarget.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class DummyTarget : StatBase
 {
     [SerializeField] private float _maxHp = 1000f;
-    [SerializeField] private float _reviveDelay = 3f;
+    [SerializeField] private float _regenDelay = 5f;
+
+    private Coroutine _regenCoroutine;
 
     protected override void Awake()
     {
@@ -12,13 +14,38 @@ public class DummyTarget : StatBase
 
         MaxHp = _maxHp;
         CurrentHp = MaxHp;
-
-        OnDie += () => StartCoroutine(ReviveAfterDelay());
     }
 
-    private IEnumerator ReviveAfterDelay()
+    // base.TakeDamage는 HP가 0이 되면 반드시 Die()를 호출하므로, 총의 실제 데미지 수치는 그대로 표시하되
+    // HP 저장값만 1로 바닥을 고정하기 위해 base를 거치지 않고 직접 처리한다
+    public override bool TakeDamage(float damage, GameObject attacker = null)
     {
-        yield return new WaitForSeconds(_reviveDelay);
-        Revive(MaxHp);
+        if (IsInvincible) return false;
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+        if (IsGodMode) return false;
+#endif
+
+        float actualDamage = damage * (1f - Mathf.Clamp01(DefenseRate));
+        CurrentHp = Mathf.Max(1f, CurrentHp - actualDamage);
+        RaiseHpChanged();
+
+        DamageTextUI.Show(actualDamage, transform.position);
+        HpWorldUI.Show(this);
+        ResetRegenTimer();
+
+        return true;
+    }
+
+    private void ResetRegenTimer()
+    {
+        if (_regenCoroutine != null)
+            StopCoroutine(_regenCoroutine);
+        _regenCoroutine = StartCoroutine(RegenAfterDelay());
+    }
+
+    private IEnumerator RegenAfterDelay()
+    {
+        yield return new WaitForSeconds(_regenDelay);
+        Heal(MaxHp);
     }
 }
